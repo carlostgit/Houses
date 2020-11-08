8bdeed884ab9961c2848abe508748fbe6660fce3
carlostgit <carlos.h.corredera@gmail.com>
2020-11-08 23:41:53 +0100

    codigo (desordenado) que permite crear nuevas casas en ejecucion

--- NewHouse.gd
+++ NewHouse.gd
@@ -12,18 +12,18 @@ var _new_building_pack = load("res://NewBuilding.tscn")
 
 var _new_building_scene:Node2D = null
 
-var _texture_button:TextureButton = null
+#var _texture_button:TextureButton = null
 
 var _world_node:Node = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_texture_button = TextureButton.new()
-	_texture_button.set_normal_texture(_house_image)
-	_texture_button.set_mouse_filter(Control.MOUSE_FILTER_IGNORE)
-	self.add_child(_texture_button)
+#	_texture_button = TextureButton.new()
+#	_texture_button.set_normal_texture(_house_image)
+#	_texture_button.set_mouse_filter(Control.MOUSE_FILTER_IGNORE)
+#	self.add_child(_texture_button)
 #	self.get_parent().get_parent().get_parent().add_child(_texture_button)
-	_texture_button.set_position(Vector2(5,5))
+#	_texture_button.set_position(Vector2(5,5))
 
 	
 	_new_building_scene = _new_building_pack.instance()
@@ -52,11 +52,11 @@ func _on_NewHouse_pressed():
 func build_if_possible():
 	if false==_move_image:
 		_move_image = true
-		self._texture_button.set_modulate(Color(1,1,1,0.25))
+#		self._texture_button.set_modulate(Color(1,1,1,0.25))
 		self._new_building_scene.set_modulate(Color(1,1,1,0.25))
 	else:
 		_move_image = false
-		self._texture_button.set_modulate(Color(1,1,1,1))
+#		self._texture_button.set_modulate(Color(1,1,1,1))
 		self._new_building_scene.set_modulate(Color(1,1,1,1))
 
 func _input(event):
@@ -65,7 +65,7 @@ func _input(event):
 	if(_move_image):
 		if event is InputEventMouseMotion:
 #			self.set_global_position(event.position)
-			self._texture_button.set_global_position(event.position)
+#			self._texture_button.set_global_position(event.position)
 			
 #			self._new_building_scene.set_global_position(event.position)
 #			var screen_coord = get_viewport_transform() * (get_global_transform() * event.position)
@@ -85,8 +85,15 @@ func _input(event):
 			if event.is_pressed():
 				if false==self._new_building_scene.is_building_blocked():
 					print("build todo")
-			
-			
+					var house:Node2D = load("res://House.tscn").instance()
+					house.set_name("Casanueva")
+					_world_node.call_deferred("add_child", house) #deferred pq _world está ocupado creando sus hijos
+					house.add_to_group("houses")
+					print(event.position)
+					var world_coord = _world_node.get_viewport_transform().affine_inverse() * event.position
+#					var local_coord = _world_node.get_global_transform().affine_inverse()*world_coord
+					house.set_global_position(world_coord)
+
 			
 
 func on_NewBuilding_area_entered(area) -> void:
